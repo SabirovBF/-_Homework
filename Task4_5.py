246a9911bb9364d56e24e4b71a0cffeaa4fcc065
SabirovBF <voribas1@gmail.com>
2024-05-03 22:31:21 +0500

    Задание выполнино

--- Task4_5.py
+++ Task4_5.py
@@ -1,24 +1,25 @@
 class Employee:
+    def get_paid(self):
+        if self.dol == "Менеджер":
+            a = 5000
+            print("Имя:", self.name, "Должность:", self.dol, "Зарплата:", a)
+        elif self.dol == "Работник":
+            a = 3000
+            print("Имя:", self.name, "Должность:", self.dol, "Зарплата:", a)
+        else:
+            print("Такой должности нет")
+class Manager(Employee):
     def __init__(self, name, dol):
         self.name = name
         self.dol = dol
 
-    def get_paid(self):
-        if self.dol == "Менеджер":
-            a = 5000
-            return f'\nИмя: {self.name}  Должность: {self.dol}  Зарплата: {a}'
-        elif self.dol == "Работник":
-            a = 3000
-            return f'\nИмя: {self.name}  Должность: {self.dol}  Зарплата: {a}'
-class Manager(Employee):
-    def get_paid(self):
-        pass
 class Worker(Employee):
-    def get_paid(self):
-        pass
+    def __init__(self, name, dol):
+        self.name = name
+        self.dol = dol
 
-manager = Employee(input("Введите имя: "), input("Введите должность: "))
-worker = Employee(input("Введите имя: "), input("Введите должность: "))
+manager = Manager(input("Введите имя: "), input("Введите должность: "))
+worker = Worker(input("Введите имя: "), input("Введите должность: "))
 
 print(manager.get_paid())
 print(worker.get_paid())
